a7632af5605c0a15550eb719c07f4c8dd516e933
Muhammad Rifal Alfarizy <mrifala29@gmail.com>
2026-02-12 13:11:48 +0700

    Update code, chatbot can get book title

--- app/agent.py
+++ app/agent.py
@@ -3,22 +3,25 @@ from pathlib import Path
 
 from langchain.agents import create_agent
 from langchain.tools import tool, ToolRuntime
+from langchain.agents.middleware import dynamic_prompt, ModelRequest
 from langchain.agents.structured_output import ToolStrategy
 
 from app.schema import Context, ResponseFormat
 
 BOOKS_DIR = Path("books")
-PROMPT_PATH = Path("prompts/system_prompt.txt")
+PROMPT_PATH = Path("prompts/booksnap_prompt.txt")
 
-def load_system_prompt() -> str:
+
+def load_prompt_template() -> str:
     if not PROMPT_PATH.exists():
-        raise FileNotFoundError("System prompt file not found.")
+        raise FileNotFoundError("booksnap_prompt.txt not found.")
     return PROMPT_PATH.read_text(encoding="utf-8")
+
 
 @tool
 def get_book_content(runtime: ToolRuntime[Context]) -> str:
     """
-    Load the full content of the selected book.
+    Load the full content of the selected book based on the current context.
     """
     filename = runtime.context.book_title.lower().replace(" ", "_") + ".txt"
     filepath = BOOKS_DIR / filename
@@ -28,11 +31,22 @@ def get_book_content(runtime: ToolRuntime[Context]) -> str:
 
     return filepath.read_text(encoding="utf-8")
 
+
+@dynamic_prompt
+def booksnap_dynamic_prompt(request: ModelRequest) -> str:
+    template = load_prompt_template()
+
+    return template.format(
+        user_id=request.runtime.context.user_id,
+        book_title=request.runtime.context.book_title,
+    )
+
+
 def build_agent(model, checkpointer):
     return create_agent(
         model=model,
-        system_prompt=load_system_prompt(),
         tools=[get_book_content],
+        middleware=[booksnap_dynamic_prompt],
         context_schema=Context,
         response_format=ToolStrategy(ResponseFormat),
         checkpointer=checkpointer,
--- app/model.py
+++ app/model.py
@@ -1,4 +1,5 @@
 import os
+from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_openai import ChatOpenAI
 from dotenv import load_dotenv
 
@@ -6,8 +7,8 @@ load_dotenv()
 
 def get_model():
     return ChatOpenAI(
-        model="mistralai/ministral-3b-2512", # use free model
+        model=os.getenv("LLM_MODEL"),
         temperature=0.5,
-        api_key=os.getenv("OPENROUTER_API_KEY"),
-        base_url="https://openrouter.ai/api/v1",
+        api_key=os.getenv("LLM_API_KEY"),
+        base_url=os.getenv("LLM_BASE_URL"),
     )
