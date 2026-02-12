f469dc146595f769ee2b3e97dbd592a9607f9c5e
Muhammad Rifal Alfarizy <mrifala29@gmail.com>
2026-02-12 15:17:36 +0700

    Final code with recommendation question

--- app/main.py
+++ app/main.py
@@ -1,4 +1,3 @@
-# app/main.py
 from fastapi import FastAPI
 from pydantic import BaseModel
 
@@ -6,6 +5,8 @@ from app.model import get_model
 from app.memory import get_checkpointer
 from app.agent import build_agent
 from app.schema import Context
+
+from langchain_core.messages import HumanMessage
 
 app = FastAPI()
 
@@ -18,9 +19,9 @@ agent = build_agent(
 
 class ChatRequest(BaseModel):
     message: str
-    username: str      
+    username: str
     book_title: str
-    summary: str       
+    summary: str
 
 @app.post("/chat")
 def chat(req: ChatRequest):
@@ -29,26 +30,33 @@ def chat(req: ChatRequest):
     response = agent.invoke(
         {"messages": [{"role": "user", "content": req.message}]},
         context=Context(
-            username=req.username,    
+            username=req.username,
             book_title=req.book_title,
-            summary=req.summary       
+            summary=req.summary
         ),
-        config={"configurable": {"thread_id": thread_id}},  
+        config={"configurable": {"thread_id": thread_id}},
     )
 
     result = response["structured_response"]
-
 
     state = checkpointer.get(
         {"configurable": {"thread_id": thread_id}}
     )
 
-    if state and len(state["channel_values"]["messages"]) <= 2:
-        result.recommendation_questions = [
-            "Who is the main character in this story?",
-            "What happens at the beginning of the story?",
-            "How does the character feel in this part of the story?"
-        ]
+    if state:
+        messages = state["channel_values"]["messages"]
+        user_messages = [m for m in messages if isinstance(m, HumanMessage)]
+
+
+        # hanya di pertanyaan pertama
+        if len(user_messages) == 1:
+            result.recommendation_questions = [
+                "Who is the main character in this story?",
+                "What happens at the beginning of the story?",
+                "How does the character feel in this part of the story?"
+            ]
+        else:
+            result.recommendation_questions = None
     else:
         result.recommendation_questions = None
 
